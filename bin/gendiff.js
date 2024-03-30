7e4312357e6512b63790f721893e11686357932e
Eugena <eugena.h12345@gmail.com>
2024-03-30 22:32:45 +0300

    add name description and vertion

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
-import getOptions from '../src/getOptions.js';
+//import getOptions from '../src/getOptions.js';
 
-getOptions();
+//getOptions();
+import { program } from 'commander';
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0');
+
+program
+
+//.option('-a, --arison', 'output the version number')
+//   .action((str, options) => {
+//     const limit = options.first ? 1 : undefined;
+//     console.log(str.split(options.separator, limit));
+//   });
+
+program.parse();
