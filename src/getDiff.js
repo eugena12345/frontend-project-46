329d65d3fa54546478d8b25737a944d8120fffa6
Eugena <eugena.h12345@gmail.com>
2024-04-02 21:51:12 +0300

    add function getDiff

---
+++ src/getDiff.js
@@ -0,0 +1,29 @@
+
+
+const getDiff = (file1, file2) => {
+    const keys1 = Object.keys(file1);
+    const keys2 = Object.keys(file2);
+    const keys = keys1.concat(keys2);
+    const uniqueKeys = [...new Set(keys)].sort();
+
+    let string = uniqueKeys.map((key) => {
+        if (keys1.includes(key)) {
+            if (keys2.includes(key)) {
+                if (file1[key] === file2[key]) {
+                    return `  ${key}: ${file1[key]}\n`;
+                } else {
+                    return `- ${key}: ${file1[key]}\n+ ${key}: ${file2[key]}\n`;
+                }
+            } else {
+                return (`- ${key}: ${file1[key]}\n`);
+            }
+        } else {
+            return `+ ${key}: ${file2[key]}\n`;
+        }
+    }).join('');
+    string = `{\n${string}}`;
+    console.log(string);
+    return string;
+};
+
+export default getDiff;
