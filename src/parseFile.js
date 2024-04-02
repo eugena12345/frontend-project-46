1d20ab00c7872600eff835b1dfa643c36f459e9c
Eugena <eugena.h12345@gmail.com>
2024-04-02 21:52:30 +0300

    add using  getDiss after parsing

--- src/parseFile.js
+++ src/parseFile.js
@@ -1,14 +1,11 @@
 import { readFileSync } from 'node:fs';
+import getDiff from './getDiff.js';
 
 const parseFiles = (f1, f2) => {
-    console.log(`f1 ${f1}, f2 ${f2}`);
-
     const data1 = readFileSync(f1);
     const result1 = JSON.parse(data1);
-    console.log(result1);
-
     const data2 = readFileSync(f2);
     const result2 = JSON.parse(data2);
-    console.log(result2);
+    getDiff(result1, result2);
 }
 export default parseFiles;
