6e41d798e2962ea3f2469689d2300d1430e4df14
Eugena <eugena.h12345@gmail.com>
2024-04-03 21:48:18 +0300

    refactoring program

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,23 +1,15 @@
 #!/usr/bin/env node
-//import getOptions from '../src/getOptions.js';
 
-//getOptions();
 import { program } from 'commander';
 import parseFiles from '../src/parseFile.js';
-
-
-
 
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
-
-program
+    .version('1.0.0')
     .arguments('<filepath1>', 'path to the file')
     .arguments('<filepath2>', 'path to the file')
     .option('-f, --format [type]', 'output format')
     .action(parseFiles);
 
-
-program.parse();
+program.parse(); 
