f0b3874c8b77965b8f0b8aa20a1a19d4820ba078
Eugena <eugena.h12345@gmail.com>
2024-04-04 21:25:36 +0300

    add getDiff.test.js

---
+++ __tests__/getDiff.test.js
@@ -0,0 +1,27 @@
+import getDiff from '../src/getDiff.js';
+
+const obj1 = {
+  host: 'hexlet.io',
+  timeout: 50,
+  proxy: '123.234.53.22',
+  follow: false,
+};
+
+const obj2 = {
+  timeout: 20,
+  verbose: true,
+  host: 'hexlet.io',
+};
+
+const result = `{
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`;
+
+test('getDiff', () => {
+  expect(getDiff(obj1, obj2)).toEqual(result);
+});
