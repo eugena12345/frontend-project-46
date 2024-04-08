e8bb2327e97a4571cebbae7c7323ad4dfe7c77b4
Eugena <eugena.h12345@gmail.com>
2024-04-08 21:26:30 +0300

    accept linter corrections

--- src/checkFiles.js
+++ src/checkFiles.js
@@ -1,9 +1,8 @@
 import getDiff from './getDiff.js';
 import parseFiles from './parseFiles.js';
 
-
 const checkFiles = (f1, f2) => {
-  const { result1, result2 } = parseFiles(f1, f2)
+  const { result1, result2 } = parseFiles(f1, f2);
   getDiff(result1, result2);
 };
 export default checkFiles;
--- src/parseFiles.js
+++ src/parseFiles.js
@@ -1,10 +1,10 @@
 import { readFileSync } from 'node:fs';
 
 const parseFiles = (f1, f2) => {
-    const data1 = readFileSync(f1);
-    const result1 = JSON.parse(data1);
-    const data2 = readFileSync(f2);
-    const result2 = JSON.parse(data2);
-    return {result1, result2}
+  const data1 = readFileSync(f1);
+  const result1 = JSON.parse(data1);
+  const data2 = readFileSync(f2);
+  const result2 = JSON.parse(data2);
+  return { result1, result2 };
 };
 export default parseFiles;
