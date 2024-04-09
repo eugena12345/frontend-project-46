1769cf2aee3784b90323565bc96dae53d9fed922
Eugena <eugena.h12345@gmail.com>
2024-04-09 21:53:00 +0300

    add console.log for print the result

--- src/checkFiles.js
+++ src/checkFiles.js
@@ -3,6 +3,6 @@ import parseFiles from '../parsers/parseFiles.js';
 
 const checkFiles = (f1, f2) => {
   const { result1, result2 } = parseFiles(f1, f2);
-  getDiff(result1, result2);
+  console.log(getDiff(result1, result2));
 };
 export default checkFiles;
