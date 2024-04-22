9ecf0fde39c373d006af93ff559b9bd2f1894d1c
Eugena <eugena.h12345@gmail.com>
2024-04-22 23:37:43 +0300

    fix spaces

--- src/formatter/renderStylish.js
+++ src/formatter/renderStylish.js
@@ -1,56 +1,60 @@
-const renderStylish = (file1, file2, space = '') => {
-    const newSpace = space + '..'
+const numberSymbol = 4;
+const numberSymbolForDelete = 2;
+
+
+const renderStylish = (file1, file2, deep = 1) => {
+    let numberRepeatSpace = numberSymbol * deep - numberSymbolForDelete;
+    if (numberRepeatSpace < 0) {
+        numberRepeatSpace = 0;
+    }
+    const space = ' '.repeat(numberRepeatSpace);
+    const spaceForEnd = ' '.repeat(numberRepeatSpace - numberSymbolForDelete);
+    const newDeep = deep + 1;
     const keys1 = Object.keys(file1);
     const keys2 = Object.keys(file2);
     const keys = keys1.concat(keys2);
     const uniqueKeys = [...new Set(keys)].sort();
-  
     let string = uniqueKeys.map((key) => {
-      let substring = `${key}: `;
-      let keyForPaint1;
-      let keyForPaint2;
-      if (keys1.includes(key)) {
-        if (keys2.includes(key)) {
-          if (typeof (file1[key]) === 'object' && typeof (file2[key]) === 'object') {
-            return `${space}  ${substring}${renderStylish(file1[key], file2[key], newSpace)}\n`
-          }
-          if (file1[key] === file2[key]) {
-            return `${space}  ${substring}${file1[key]}\n`;
-          }
-          if (typeof (file1[key]) === 'object' && file1[key] !== null) {
-            keyForPaint1 = renderStylish(file1[key], file1[key], newSpace);
-          } else {
-            keyForPaint1 = file1[key]
-          }
-          if (typeof (file2[key]) === 'object' && file2[key] !== null) {
-            console.log(`file2[key]${file2[key]}`);
-            keyForPaint2 = renderStylish(file2[key], file2[key], newSpace);
-          } else {
-            keyForPaint2 = file2[key]
-          }
-          return `${space}- ${substring}${keyForPaint1}\n${space}+ ${key}: ${keyForPaint2}\n`;
+        let substring = `${key}: `;
+        let keyForPaint1;
+        let keyForPaint2;
+        if (keys1.includes(key)) {
+            if (keys2.includes(key)) {
+                if (typeof (file1[key]) === 'object' && typeof (file2[key]) === 'object') {
+                    return `${space}  ${substring}${renderStylish(file1[key], file2[key], newDeep)}\n`
+                }
+                if (file1[key] === file2[key]) {
+                    return `${space}  ${substring}${file1[key]}\n`;
+                }
+                if (typeof (file1[key]) === 'object' && file1[key] !== null) {
+                    keyForPaint1 = renderStylish(file1[key], file1[key], newDeep);
+                } else {
+                    keyForPaint1 = file1[key]
+                }
+                if (typeof (file2[key]) === 'object' && file2[key] !== null) {
+                    console.log(`file2[key]${file2[key]}`);
+                    keyForPaint2 = renderStylish(file2[key], file2[key], newDeep);
+                } else {
+                    keyForPaint2 = file2[key]
+                }
+                return `${space}- ${substring}${keyForPaint1}\n${space}+ ${key}: ${keyForPaint2}\n`;
+            }
+
+            if (typeof (file1[key]) === 'object' && file1[key] !== null) {
+                keyForPaint1 = renderStylish(file1[key], file1[key], newDeep);
+            } else {
+                keyForPaint1 = file1[key]
+            }
+            return `${space}- ${substring}${keyForPaint1}\n`;
         }
-  
-        if (typeof (file1[key]) === 'object' && file1[key] !== null) {
-          keyForPaint1 = renderStylish(file1[key], file1[key], newSpace);
+        if (typeof (file2[key]) === 'object' && file2[key] !== null) {
+            keyForPaint2 = renderStylish(file2[key], file2[key], newDeep);
         } else {
-          keyForPaint1 = file1[key]
+            keyForPaint2 = file2[key]
         }
-        return `${space}- ${substring}${keyForPaint1}\n`;
-      }
-      if (typeof (file2[key]) === 'object'&& file2[key] !== null) {
-        keyForPaint2 = renderStylish(file2[key], file2[key], newSpace);
-      } else {
-        keyForPaint2 = file2[key]
-      }
-      return `${space}+ ${substring}${keyForPaint2}\n`;
+        return `${space}+ ${substring}${keyForPaint2}\n`;
     }).join('');
-  
-    //string = `{\n${string}}`;
-    // console.log(`${string} \nend------------------`);
-    return `{\n${string}${space}}`;
-  
-  
-  };
-  
-  export default renderStylish;
+    return `{\n${string}${spaceForEnd}}`;
+};
+
+export default renderStylish;
