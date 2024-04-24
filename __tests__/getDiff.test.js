75f1bc3f8d6e097dd2f154efa9b56d4bff0eef50
Eugena <eugena.h12345@gmail.com>
2024-04-24 19:31:08 +0300

    fix test problems

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -1,73 +1,73 @@
 import { test, expect } from '@jest/globals';
 import checkFiles from '../src/checkFiles.js';
 
-const obj1 = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
+// const obj1 = {
+//   host: 'hexlet.io',
+//   timeout: 50,
+//   proxy: '123.234.53.22',
+//   follow: false,
+// };
 
-const obj2 = {
-  timeout: 20,
-  verbose: true,
-  host: 'hexlet.io',
-};
+// const obj2 = {
+//   timeout: 20,
+//   verbose: true,
+//   host: 'hexlet.io',
+// };
 
-const result = `{
-- follow: false
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true
-}`;
-
-// const result2 = `{
-//     common: {
-//       + follow: false
-//         setting1: Value 1
-//       - setting2: 200
-//       - setting3: true
-//       + setting3: null
-//       + setting4: blah blah
-//       + setting5: {
-//             key5: value5
-//         }
-//         setting6: {
-//             doge: {
-//               - wow:
-//               + wow: so much
-//             }
-//             key: value
-//           + ops: vops
-//         }
-//     }
-//     group1: {
-//       - baz: bas
-//       + baz: bars
-//         foo: bar
-//       - nest: {
-//             key: value
-//         }
-//       + nest: str
-//     }
-//   - group2: {
-//         abc: 12345
-//         deep: {
-//             id: 45
-//         }
-//     }
-//   + group3: {
-//         deep: {
-//             id: {
-//                 number: 45
-//             }
-//         }
-//         fee: 100500
-//     }
+// const result = `{
+// - follow: false
+//   host: hexlet.io
+// - proxy: 123.234.53.22
+// - timeout: 50
+// + timeout: 20
+// + verbose: true
 // }`;
 
+const result2 = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
+
 test('checkFiles', () => {
-  expect(checkFiles(obj1, obj2, 'stylish')).toEqual(result);
+  expect(checkFiles('__tests__/__fixtures__/file1.json', '__tests__/__fixtures__/file2.json', { format: 'stylish' })).toEqual(result2);
 });
--- src/checkFiles.js
+++ src/checkFiles.js
@@ -4,5 +4,6 @@ import render from './formatter/index.js';
 const checkFiles = (f1, f2, option) => {
   const { result1, result2 } = parseFiles(f1, f2);
   console.log(render(option.format, result1, result2));
+  return (render(option.format, result1, result2));
 };
 export default checkFiles;
