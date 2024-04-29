4a3d4ca028b5348c4bb0b847f0e03cf3472a1c7b
Eugena <eugena.h12345@gmail.com>
2024-04-29 21:19:30 +0300

    add options plain

--- src/formatter/index.js
+++ src/formatter/index.js
@@ -1,7 +1,9 @@
 import renderStylish from './renderStylish.js';
+import renderPlain from './renderPlain.js';
 
 const actions = {
   stylish: renderStylish,
+  plain: renderPlain,
 };
 
 export default (format, different) => actions[format](different);
