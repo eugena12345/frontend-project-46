0922b9606b43e239a856853d4d4baaffcb0d1962
Eugena <eugena.h12345@gmail.com>
2024-05-07 21:54:03 +0300

    add path.resolve

--- parsers/parseFiles.js
+++ parsers/parseFiles.js
@@ -1,5 +1,6 @@
 import { readFileSync } from 'node:fs';
 import yaml from 'js-yaml';
+import path from 'node:path';
 
 const getExtension = (filePath) => {
   const separetedPath = filePath.split('.');
@@ -18,10 +19,13 @@ const getParsedFileByExtention = (extention, data) => {
 };
 
 const parseFiles = (f1, f2) => {
+  const resolvedPath1 = path.resolve(f1);
+  const resolvedPath2 = path.resolve(f2);
+
   const extFile1 = getExtension(f1);
   const extFile2 = getExtension(f2);
-  const data1 = readFileSync(f1);
-  const data2 = readFileSync(f2);
+  const data1 = readFileSync(resolvedPath1);
+  const data2 = readFileSync(resolvedPath2);
   const result1 = getParsedFileByExtention(extFile1, data1);
   const result2 = getParsedFileByExtention(extFile2, data2);
 
