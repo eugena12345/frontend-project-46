71e06fc71000aadac97d4b014625af479469d74e
Eugena <eugena.h12345@gmail.com>
2024-05-07 20:00:31 +0300

    add default formatter

--- src/checkFiles.js
+++ src/checkFiles.js
@@ -2,7 +2,7 @@ import parseFiles from '../parsers/parseFiles.js';
 import findDiff from './findDiff.js';
 import render from './formatter/index.js';
 
-const checkFiles = (f1, f2, option) => {
+const checkFiles = (f1, f2, option = { format: 'stylish' }) => {
   const { result1, result2 } = parseFiles(f1, f2);
   const different = findDiff(result1, result2);
   console.log(render(option.format, different));
