6703d7c017e76941b4ea98f0e4cb5c5b5717e63a
Eugena <eugena.h12345@gmail.com>
2024-05-12 17:49:07 +0300

    add console.log to see more

--- parsers/parseFiles.js
+++ parsers/parseFiles.js
@@ -21,13 +21,16 @@ const getParsedFileByExtention = (extention, data) => {
 const parseFiles = (f1, f2) => {
   const resolvedPath1 = path.resolve(process.cwd(), '__tests__', f1);
   const resolvedPath2 = path.resolve(process.cwd(), '__tests__', f2);
-  console.log(`resolvedPath1 ${resolvedPath1}`);
-  console.log(`resolvedPath2 ${resolvedPath2}`);
+  // console.log(`resolvedPath1 ${resolvedPath1}`);
+  // console.log(`resolvedPath2 ${resolvedPath2}`);
 
   const extFile1 = getExtension(f1);
   const extFile2 = getExtension(f2);
   const data1 = readFileSync(resolvedPath1);
   const data2 = readFileSync(resolvedPath2);
+  console.log(data1);
+  console.log(data2);
+
   const result1 = getParsedFileByExtention(extFile1, data1);
   const result2 = getParsedFileByExtention(extFile2, data2);
 
