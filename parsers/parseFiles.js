7234966cd5f0fb01cd34787e5e2126d9f7b09f37
Eugena <eugena.h12345@gmail.com>
2024-05-12 17:27:57 +0300

    want to look at path

--- parsers/parseFiles.js
+++ parsers/parseFiles.js
@@ -21,6 +21,8 @@ const getParsedFileByExtention = (extention, data) => {
 const parseFiles = (f1, f2) => {
   const resolvedPath1 = path.resolve(process.cwd(), f1);
   const resolvedPath2 = path.resolve(process.cwd(), f2);
+  console.log(`resolvedPath1 ${resolvedPath1}`);
+  console.log(`resolvedPath1 ${resolvedPath1}`);
 
   const extFile1 = getExtension(f1);
   const extFile2 = getExtension(f2);
--- src/checkFiles.js
+++ src/checkFiles.js
@@ -5,7 +5,7 @@ import render from './formatter/index.js';
 const checkFiles = (f1, f2, option = { format: 'stylish' }) => {
   const { result1, result2 } = parseFiles(f1, f2);
   const different = findDiff(result1, result2);
-  // (render(option.format, different));
-  return console.log(render(option.format, different));
+  // console.log(render(option.format, different));
+  return render(option.format, different);
 };
 export default checkFiles;
