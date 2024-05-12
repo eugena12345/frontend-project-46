3f3378aed43083298a7d60f0a7e16793115d84fa
Eugena <eugena.h12345@gmail.com>
2024-05-12 18:34:08 +0300

    experimrnt with default parametr

--- src/checkFiles.js
+++ src/checkFiles.js
@@ -5,8 +5,8 @@ import render from './formatter/index.js';
 const checkFiles = (f1, f2, option = { format: 'stylish' }) => {
   const { result1, result2 } = parseFiles(f1, f2);
   const different = findDiff(result1, result2);
-  console.log(`option ${option}`);
+  console.log(`option ${option}`); // возвращает 'stylish' #58 буз формата
   // console.log(render(option.format, different));
-  return render(different, option.format);
+  return render(different, option.format || option);
 };
 export default checkFiles;
