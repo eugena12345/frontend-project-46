23f42f92412000752b3404af233db0098510a517
Eugena <eugena.h12345@gmail.com>
2024-05-21 21:42:27 +0300

    add func for getting folepath

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -1,14 +1,17 @@
 import { test, expect } from '@jest/globals';
 import fs from 'fs';
+import path from 'node:path';
 import checkFiles from '../src/checkFiles.js';
 
-const resultStylish = fs.readFileSync('__tests__/__fixtures__/stylishResult', 'utf-8');
-const resultPlain = fs.readFileSync('__tests__/__fixtures__/plainResult', 'utf-8');
+const getFilePath = (fileName) => path.resolve(process.cwd(), '__tests__', '__fixtures__', fileName);
+
+const resultStylish = fs.readFileSync(getFilePath('stylishResult'), 'utf-8');
+const resultPlain = fs.readFileSync(getFilePath('plainResult'), 'utf-8');
 const fileExtention = ['yaml', 'json'];
 
 fileExtention.forEach((ext) => {
-  const pathFile1 = `__fixtures__/file1.${ext}`;
-  const pathFile2 = `__fixtures__/file2.${ext}`;
+  const pathFile1 = getFilePath(`file1.${ext}`);
+  const pathFile2 = getFilePath(`file2.${ext}`);
 
   test(`${ext} test stylish format`, () => {
     expect(checkFiles(pathFile1, pathFile2, { format: 'stylish' })).toEqual(resultStylish);
