f42f85b7a2a27376cb03fd02ccf0b88cfd7f00e2
Eugena <eugena.h12345@gmail.com>
2024-05-22 21:54:14 +0300

    add createNode func refactoring

--- src/findDiff.js
+++ src/findDiff.js
@@ -3,6 +3,14 @@ import has from 'lodash/has.js';
 import sortBy from 'lodash/sortBy.js';
 import isObject from 'lodash/isObject.js';
 
+const createNode = (obj1, obj2, key, type, children = null) => ({
+  name: key,
+  type,
+  beforeValue: obj1[key],
+  afterValue: obj2[key],
+  children,
+});
+
 const findDiff = (obj1, obj2) => {
   const key1 = Object.keys(obj1 ?? {});
   const key2 = Object.keys(obj2 ?? {});
@@ -10,50 +18,20 @@ const findDiff = (obj1, obj2) => {
   const sortedKeys1 = sortBy(commonKeys);
   const result = sortedKeys1.map((key) => {
     if (!has(obj2, key)) {
-      return {
-        name: key,
-        type: 'removed',
-        beforeValue: obj1[key],
-        afterValue: obj2[key],
-        children: null,
-      };
+      return createNode(obj1, obj2, key, 'removed');
     }
     if (!has(obj1, key)) {
-      return {
-        name: key,
-        type: 'added',
-        beforeValue: obj1[key],
-        afterValue: obj2[key],
-        children: null,
-      };
+      return createNode(obj1, obj2, key, 'added');
     }
     if (obj1[key] === obj2[key]) {
-      return {
-        name: key,
-        type: 'unchanged',
-        beforeValue: obj1[key],
-        afterValue: obj2[key],
-        children: null,
-      };
+      return createNode(obj1, obj2, key, 'unchanged');
     }
     if (isObject(obj1[key]) && isObject(obj2[key])) {
-      // (typeof (obj1[key]) === 'object' && typeof (obj2[key]) === 'object'
-      // && obj1[key] !== null && obj1[key] !== null)
-      return {
-        name: key,
-        type: 'nested',
-        beforeValue: obj1[key],
-        afterValue: obj2[key],
-        children: findDiff(obj1[key], obj2[key]),
-      };
+      const children = findDiff(obj1[key], obj2[key]);
+      console.log(children);
+      return createNode(obj1, obj2, key, 'nested', children);
     }
-    return {
-      name: key,
-      type: 'changed',
-      beforeValue: obj1[key],
-      afterValue: obj2[key],
-      children: null,
-    };
+    return createNode(obj1, obj2, key, 'changed');
   });
   return result;
 };
